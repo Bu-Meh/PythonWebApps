349b6180143c1ac9ad1c74c1da040e92f5ef8188
Bu-Meh <meh5782@bears.unco.edu>
2022-11-27 14:22:06 -0700

    updates

--- Superhero13/config/hero/models.py
+++ Superhero13/config/hero/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+
+class Hero(models.Model):
+    name = models.CharField(max_length=200, default='name')
+    strengths = models.TextField(max_length=500, default='strengths')
+    weaknesses = models.TextField(max_length=500, default='weaknesses')
+    identity = models.TextField(max_length=200, default='identity')
+    description = models.TextField(max_length=2000, default='description')
+    notes = models.TextField(max_length=2000, default='notes')
+    photo = models.ImageField(null=True, blank=True, upload_to="images/")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse_lazy('hero_list')
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, editable=False)
+    bio = models.TextField()
+
+    def __str__(self):
+        return f'{self.user.username}'
+
+
+class Photo (models.Model):
+
+    hero = models.ForeignKey(Author, on_delete=models.CASCADE, editable=False)
+    title = models.CharField(max_length=100)
+    image = models.ImageField(null=True, blank=True, upload_to="images/")
+
+    def __str__(self):
+        return f'{self.pk} - {self.title}'
+
+    def get_absolute_url(self):
+        return reverse_lazy('photo_detail', args=[str(self.id)])
--- Superhero13/config/hero/views.py
+++ Superhero13/config/hero/views.py
@@ -1,3 +1,61 @@
 from django.shortcuts import render
 
 # Create your views here.
+from pathlib import Path
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView, DeleteView, UpdateView
+from .models import Hero
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class PhotoListView(TemplateView):
+    template_name = 'heroes.html'
+    def get_context_data(self, **kwargs):
+        return {
+            'object_list': Hero.objects.all()
+        }
+
+class NotesView(TemplateView):
+    template_name = 'notes.html'
+    def get_context_data(self, **kwargs):
+        return {
+            'object_list': Hero.objects.all()
+        }
+
+class CarouselView(TemplateView):
+    template_name = 'Carousel.html'
+    def get_context_data(self, **kwargs):
+        return {
+            'object_list': Hero.objects.all()
+        }
+
+class PhotoDetailView(TemplateView):
+    template_name = 'photo.html'
+
+    def get_context_data(self, **kwargs):
+        i = kwargs['id']
+        return dict(hero=Hero.objects.get(pk=i))
+
+
+class HeroCreateView(LoginRequiredMixin, CreateView):  
+    template_name = "add.html"
+    model = Hero
+    fields = '__all__'
+
+class HeroUpdateView(LoginRequiredMixin, UpdateView):
+    template_name = "edit.html"
+    model = Hero
+    fields = '__all__'
+
+
+class HeroDeleteView(LoginRequiredMixin, DeleteView):
+    model = Hero
+    template_name = 'delete.html'
+    success_url = reverse_lazy('hero_list')
+
+class AuthorAddView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/account_add.html'
